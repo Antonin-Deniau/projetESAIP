a5ad8e81b65fe59c0f8237ba258d4f14dd07b57e
Antonin Deniau <antonin.deniau@gmail.com>
2016-12-05 15:35:26 +0100

    Init

---
+++ src/main/java/com/projetESAIP/ProjetEsaipApplication.java
@@ -0,0 +1,12 @@
+package com.projetESAIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEsaipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEsaipApplication.class, args);
+	}
+}
